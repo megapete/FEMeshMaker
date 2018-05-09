f796affac558fc677fa7861b9f6621b77d317696
Peter Huber <peterhub@peters-macbook-pro.local>
2018-05-09 19:59:02 -0400

    Working on RHS calculation for magnetostatics (flat)

--- FEMeshMaker/FlatMagnetostaticComplexPotentialMesh.swift
+++ FEMeshMaker/FlatMagnetostaticComplexPotentialMesh.swift
@@ -11,9 +11,22 @@ import Foundation
 
 class FlatMagnetostaticComplexPotentialMesh: FE_Mesh
 {
+    var magneticBoundaries:[Int:MagneticBoundary] = [:]
+    
     init(withPaths:[MeshPath], units:FE_Mesh.Units, vertices:[NSPoint], regions:[Region], holes:[NSPoint] = [])
     {
         super.init(precision: .complex, units: units, withPaths: withPaths, vertices: vertices, regions: regions)
+        
+        for nextPath in withPaths
+        {
+            if let boundary = nextPath.boundary
+            {
+                if let magBoundary = boundary as? MagneticBoundary
+                {
+                    magneticBoundaries[magBoundary.tag] = magBoundary
+                }
+            }
+        }
         
         self.Setup_A_Matrix()
         self.SetupComplexBmatrix()
@@ -33,11 +46,73 @@ class FlatMagnetostaticComplexPotentialMesh: FE_Mesh
     
     override func CalculateCouplingConstants(node:Node)
     {
+        if node.marker != 0 && node.marker != Boundary.neumannTagNumber
+        {
+            if self.magneticBoundaries[node.marker] != nil
+            {
+                self.matrixA![node.tag, node.tag] = Complex(real: 1.0)
+                return
+            }
+            else
+            {
+                ALog("Could not find boundary in dictionary!")
+                return
+            }
+        }
         
+        // It's a regular node, so we do Humphries Eq. 9.54
+        var sumWi = Complex(real: 0.0)
+        
+        for triangle in node.elements
+        {
+            let nextTriangle = triangle.NormalizedOn(n0: node)
+            
+            let colIndexN2 = nextTriangle.corners.n2.tag // for the first triangle, this is labeled 𝜙1 in Humphries
+            let colIndexN1 = nextTriangle.corners.n1.tag // for the first triangle, this is labeled 𝜙6 in Humphries
+            
+            var µr = Complex(real: 1.0)
+            if let region = nextTriangle.region
+            {
+                µr = region.µRel
+            }
+            
+            let cotanA = nextTriangle.CotanThetaA()
+            
+            let coeffN2 = Complex(real: cotanA / (µr.real * 2.0), imag:  cotanA / (µr.imag * 2.0))
+            
+            let cotanB = nextTriangle.CotanThetaB()
+            let coeffN1 = Complex(real: cotanB / (µr.real * 2.0), imag:  cotanB / (µr.imag * 2.0))
+            
+            sumWi += coeffN1 + coeffN2
+            
+            let prevN2:Complex = self.matrixA![node.tag, colIndexN2]
+            self.matrixA![node.tag, colIndexN2] = Complex(real: prevN2.real - coeffN2.real, imag: prevN2.imag - coeffN2.imag)
+            
+            let prevN1:Complex = self.matrixA![node.tag, colIndexN1]
+            self.matrixA![node.tag, colIndexN1] = Complex(real: prevN1.real - coeffN1.real, imag: prevN1.imag - coeffN1.imag)
+        }
+        
+        self.matrixA![node.tag, node.tag] = sumWi
     }
     
     override func CalculateRHSforNode(node:Node)
     {
+        if node.marker != 0 && node.marker != Boundary.neumannTagNumber
+        {
+            if let magBound = self.magneticBoundaries[node.marker]
+            {
+                self.complexMatrixB[node.tag] = magBound.prescribedPotential
+                return
+            }
+            else
+            {
+                ALog("Could not find boundary in dictionary!")
+                return
+            }
+        }
         
+        var result = Complex(real: 0.0)
+        let µFactor = (self.units == .mm ? 0.001 : 0.001 * 25.4)
+        let constant = Complex(real: 1.0 / (3.0 * µ0 * µFactor))
     }
 }
